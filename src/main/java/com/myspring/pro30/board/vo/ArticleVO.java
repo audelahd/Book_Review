b62cef8ece2889bd80bad43d9c9246dfd0aa5a52
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-07-24 17:48:24 +0900

    Spring으로 게시판 기능 구현하기 - 방향성 잡고 JSP / DB 관련 소스 수정 (진행중)

---
+++ src/main/java/com/myspring/pro30/board/vo/ArticleVO.java
@@ -0,0 +1,119 @@
+package com.myspring.pro30.board.vo;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component("articleVO")
+public class ArticleVO {
+	private int  level; //글 레벨
+	private int articleNO; //글 번호
+	private int parentNO; //의견
+	private float staring; //별점
+	private String booktitle; //책 제목
+	private String content; //리뷰 내용
+	private String imageFileName; //이미지 파일
+	private String id; //작성자
+	private Date  writeDate; //작성일 
+	
+	
+	public ArticleVO() {
+		System.out.println("ArticleVO ������");
+	}
+
+	public int getArticleNO() {
+		return articleNO;
+	}
+
+	public void setArticleNO(int articleNO) {
+		this.articleNO = articleNO;
+	}
+
+	public int getParentNO() {
+		return parentNO;
+	}
+
+	public void setParentNO(int parentNO) {
+		this.parentNO = parentNO;
+	}
+	
+	
+
+	public float getStaring() {
+		return staring;
+	}
+
+	public void setStaring(float staring) {
+		this.staring = staring;
+	}
+
+	public String getBooktitle() {
+		return booktitle;
+	}
+
+	public void setBooktitle(String booktitle) {
+		this.booktitle = booktitle;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImageFileName() {
+		try {
+			if (imageFileName != null && imageFileName.length() != 0) {
+				imageFileName = URLDecoder.decode(imageFileName, "UTF-8");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		try {
+			if(imageFileName!= null && imageFileName.length()!=0) {
+				this.imageFileName = URLEncoder.encode(imageFileName,"UTF-8");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getWriteDate() {
+		return writeDate;
+	}
+
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+
+
+	
+	
+}
