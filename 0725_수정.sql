ff5f927ad17ec159b423fd735771debc5ce3b530
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-07-25 17:41:19 +0900

    SQL문 추가 수정

--- 0725_수정.sql
+++ 0725_수정.sql
@@ -1,6 +1,6 @@
 drop table t_board;
 drop table t_member;
-drop table t_tag;
+drop table t_book;
 
 create table t_member(
     id varchar2(10) primary key,
@@ -10,6 +10,14 @@ create table t_member(
     joinDate date default sysdate
 );
 
+CREATE TABLE t_book(
+    booknum int,
+    booktitle VARCHAR2(500) NOT NULL,
+    bookwriter VARCHAR2(100) NOT NULL,
+    genre VARCHAR2(100) NOT NULL,
+
+    PRIMARY KEY (booknum)
+);
 
 CREATE TABLE t_board (
     articleNO NUMBER(10) primary key,
@@ -19,8 +27,7 @@ CREATE TABLE t_board (
     writeDate DATE default SYSDATE NOT NULL,
     id VARCHAR2(10),
     CONSTRAINT FK_ID FOREIGN KEY(id) REFERENCES t_member(id),
-    booktitle VARCHAR2(500),
-    tagname VARCHAR2(100)
+    booknum int REFERENCES t_book (booknum)
 );
 
 insert into t_member
@@ -35,21 +42,35 @@ values('kim', '1212', '김유신', 'kimg@gmail.com', sysdate);
 insert into t_member
 values('park', '1212', '박찬욱', 'park@gmail.com', sysdate); 
 
-insert into t_board(articleNO, parentNO, booktitle, content, staring,  writeDate, id)
-values(1, 0, '노인과 바다', '좋은 책입니다', 2.5, sysdate, 'hong');
+insert into t_book
+values(1,'book1', 'writer1', 'genre1');
+insert into t_book
+values(2,'book2', 'writer2', 'genre2');
+insert into t_book
+values(3,'book3', 'writer3', 'genre3');
+insert into t_book
+values(4,'book4', 'writer4', 'genre4');
+insert into t_book
+values(5,'book5', 'writer5', 'genre5');
 
-insert into t_board(articleNO, parentNO, booktitle, content, staring,  writeDate, id)
-values(2, 0, '호두까기 인형', '아이들이 읽기 좋습니다', 3.5, sysdate, 'park');
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(1, 0, 1, '좋은 책입니다', 2.5, sysdate, 'hong');
 
-insert into t_board(articleNO, parentNO, booktitle, content, staring,  writeDate, id)
-values(3, 0, '아기판다 푸바오', '판다가 귀엽습니다', 4.0, sysdate, 'park');
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(2, 0, 2, '아이들이 읽기 좋습니다', 3.5, sysdate, 'park');
 
-insert into t_board(articleNO, parentNO, booktitle, content, staring,  writeDate, id)
-values(4, 0, '도둑맞은 집중력', '도움이 되는 책입니다', 3.0, sysdate, 'lee');
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(3, 0, 3, '판다가 귀엽습니다', 4.0, sysdate, 'park');
 
-insert into t_board(articleNO, parentNO, booktitle, content, staring,  writeDate, id)
-values(5, 0, '별로인 책', '생각보다 별로입니다', 1.0, sysdate, 'kim');
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(4, 0, 4, '도움이 되는 책입니다', 3.0, sysdate, 'lee');
+
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(5, 0, 5, '생각보다 별로입니다', 1.0, sysdate, 'kim');
+
+insert into t_board(articleNO, parentNO, booknum, content, staring,  writeDate, id)
+values(6, 0, 5, '생각보다 더 별로입니다', 1.5, sysdate, 'lee');
 
 commit;
-select*from t_board;
-select*from t_tag;
+
+select * from t_Board;
